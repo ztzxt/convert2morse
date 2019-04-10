28ea72e6339f4b4b8c9ee7e5c205b6cbe6d5288a
ztzxt <ztzxt@yandex.com>
2019-04-10 22:59:01 +0300

    Update server.js

--- public/server.js
+++ public/server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const fs = require('fs');
-
 
 const latinToMorse = {
     'A': '.-',
